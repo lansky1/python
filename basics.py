306734baa5ce5aa14ce182586294cc53ae3dbbb4
Ekalavya <77741579+lansky1@users.noreply.github.com>
2026-05-01 22:21:41 +0530

    docs: added strings

--- basics.py
+++ basics.py
@@ -3,13 +3,8 @@
 # Batteries Included Philosophy
 # Reference: https://peps.python.org/pep-0206/
 
-# Lists in Python
-# TODO: How is a list implemented internally?
-# example_list = [10, "hello", 200.3]
-# print(example_list)
-
 # Division in Python
-# '/' always performs floating-point division.
+# '/' always performs floating-point division.~
 # '//' performs integer division.
 # print(5 // 2)
 
@@ -25,9 +20,32 @@
 # 001^110 = 111
 
 # Strings
-# my_string = "Hello"
+# my_string = "Hello"~
 # print(my_string[::-2]) #Slice
 # print(my_string[5])
 # print("Hello World"[8])
-letter = 'a'
-print(letter*10)
+# letter = 'a'
+# print(letter*10)
+
+# String Formatting
+# print("This is a string {}".format("Inserted"))
+# print("The {2} {1} {0}".format("fox", "brown", "quick"))
+# print("The {q} {b} {f}".format(f="fox", b="brown", q="quick"))
+# print("The {} {} {}".format(f="fox", b="brown", q="quick"))
+# {} are postitional placeholders. f=, b= are keyword arguments. 
+# Both are incompatible.
+
+# Float Manipulation 
+# value:width.precision f
+# print("The answer is {:1.1f}".format(100/77))
+
+# F Strings
+# name = "John"
+# print(f"Hello, my name is {name}")
+# Reference: https://pyformat.info
+
+# Lists in Python
+# TODO: How is a list implemented internally?
+example_list = [10, "hello", 200.3]
+# print(example_list)
+print(example_list[1][3])
